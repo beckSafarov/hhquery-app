3dd0e17c0ae10538fcce6d6a476e892d4bb16a9c
Begzod Safarov <begzod.safarov07@gmail.com>
2025-04-15 10:54:15 +0500

    bug fixed: in the emps tab chart labels are not syncing with the language

--- components/display_employers_tab.py
+++ components/display_employers_tab.py
@@ -1,25 +1,42 @@
 import streamlit as st #type:ignore
-from utils.plots import plot_vbar,plot_hbar
-from data.plot_labels import emps_avg_salary,emps_most_vacancy
+from utils.plots import plot_vbar, plot_hbar
 from utils.get_text import get_translated_text as t
 
 
 def display_top_employers_with_vacancies(employer_df):
-  if len(employer_df) < 1:
-    return st.write('No Stats found for top employers by vacancies')
-  caption = t('chart_captions.top_employers_vacancies')
-  print(f'caption top emps with vacs: {caption}')
-  emp_vacs = plot_vbar(employer_df, caption, emps_most_vacancy,group_by='employer_name',id='id')
-  st.plotly_chart(emp_vacs)
+    if len(employer_df) < 1:
+        return st.write("No Stats found for top employers by vacancies")
+    caption = t("chart_captions.top_employers_vacancies")
+    labels = {
+        "employer_name": t("chart_labels.emps_most_vacancies.employer_name"),
+        "y": t("chart_labels.emps_most_vacancies.y"),
+    }
+    emp_vacs = plot_vbar(
+        employer_df, caption, labels, group_by="employer_name", id="id"
+    )
+    st.plotly_chart(emp_vacs)
 
 def display_top_employers_by_avg_salary(employer_df,salary_df):
-  if len(salary_df) < 1 or len(employer_df) < 1:
-    return st.write('No Stats found for top employers by average salaries')
-  merged = salary_df.merge(employer_df,how='inner',left_on='employer_id',right_on='id')
-  caption = t('chart_captions.top_paying_employers')
-  print(f'caption top paying employers: {caption}')
-  emp_sals = plot_hbar(merged,'employer_name','average',caption,emps_avg_salary,top_count=10,aggregation_method='mean',)
-  st.plotly_chart(emp_sals)
+    if len(salary_df) < 1 or len(employer_df) < 1:
+        return st.write("No Stats found for top employers by average salaries")
+    merged = salary_df.merge(
+        employer_df, how="inner", left_on="employer_id", right_on="id"
+    )
+    caption = t("chart_captions.top_paying_employers")
+    labels = {
+        "x": t("chart_labels.emps_top_avg_salary.x"),
+        "y": t("chart_labels.emps_top_avg_salary.y"),
+    }
+    emp_sals = plot_hbar(
+        merged,
+        "employer_name",
+        "average",
+        caption,
+        labels,
+        top_count=10,
+        aggregation_method="mean",
+    )
+    st.plotly_chart(emp_sals)
 
 def display_employers_tab():
   employer_df = st.session_state.employer_df
--- data/plot_labels.py
+++
@@ -1,5 +0,0 @@
-from utils.get_text import get_translated_text as t
-import streamlit as st #type: ignore
-
-emps_most_vacancy = {'employer_name':t('chart_labels.emps_most_vacancies.employer_name'), 'y':t('chart_labels.emps_most_vacancies.y')}
-emps_avg_salary = {'x':t('chart_labels.emps_top_avg_salary.x'), 'y':t('chart_labels.emps_top_avg_salary.y')}
